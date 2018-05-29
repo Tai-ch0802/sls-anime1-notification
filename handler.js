786f1f8707ae032dd4e0d417fed1d17c22d56bd6
Tai-chunchien <i0927935851@gmail.com>
2018-05-29 14:40:17 +0800

    first commit

---
+++ handler.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports.helloWorld = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+    },
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      webhook: process.env.SLACK_WEBHOOK_URL
+    }),
+  };
+
+  callback(null, response);
+};

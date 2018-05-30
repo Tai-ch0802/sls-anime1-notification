8b8e77c6c82ce582023dbf92fbe13d330a7afe38
Tai-chunchien <i0927935851@gmail.com>
2018-05-30 10:05:06 +0800

    Build the crawler

---
+++ test/crawlerServiceTest.js
@@ -0,0 +1,9 @@
+const should = require('should')
+const service = require('../lib/services/crawlerService')
+
+describe('#get()',() => {
+    it('should return the list', done => {
+        var list = service.getListFromAnime1();
+        done();
+    })
+})
